1a48a733676b7bf5be7230a6f650b66248a0618e
hirleog <87393381+hirleog@users.noreply.github.com>
2023-01-08 20:05:57 -0300

    authorization components added. Strategy and guards added too

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { LocalAuthGuard } from './guards/local-auth.guard';
+import { AuthService } from './auth.service';
+import { Controller, HttpCode, Post, HttpStatus, UseGuards } from '@nestjs/common';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+
+    constructor(private readonly AuthService: AuthService) { }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    @UseGuards(LocalAuthGuard)
+    login () {
+        // return 'login successfully'
+    }
+}
